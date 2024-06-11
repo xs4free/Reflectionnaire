2a380b7560023440fb1cf3774fee15342ed36a99
Rogier Reedijk <github@progz.nl>
2024-06-11 23:06:23 +0200

    Addd QRCode and refresh-button

--- Client/Pages/Manage/ReflectionnaireReport.razor.cs
+++ Client/Pages/Manage/ReflectionnaireReport.razor.cs
@@ -1,13 +1,17 @@
 ﻿using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using QRCoder;
 using Reflectionnaire.Shared;
+using static QRCoder.PayloadGenerator;
 
 namespace Reflectionnaire.Client.Pages.Manage;
 
 public partial class ReflectionnaireReport
 {
-    [Inject] private HttpClient ReflectionnaireService { get; set; }
-    [Inject] private ILogger<ReflectionnaireReport> Logger { get; set; }
+    [Inject] private HttpClient ReflectionnaireService { get; set; } = null!;
+    [Inject] private ILogger<ReflectionnaireReport> Logger { get; set; } = null!;
+    [Inject] IJSRuntime JSRuntime { get; set; } = null!;
     [Parameter] public Guid? ReflectionnaireId { get; set; }
 
     private ReflectionnaireAllUserAnswers? _reflectionnaire;
@@ -16,8 +20,19 @@ public partial class ReflectionnaireReport
     private float _score2 = 0;
     private float _score3 = 0;
     private float _score4 = 0;
+    private string _qrBytes = "";
+    private string _urlReflectionnaire = "";
 
     protected override async Task OnInitializedAsync()
+    {
+        await UpdateRadarChart();
+
+        _urlReflectionnaire = $"https://www.reflectionnaire.com/questions/{ReflectionnaireId}";
+
+        GenerateQRCode(_urlReflectionnaire);
+    }
+
+    private async Task UpdateRadarChart()
     {
         try
         {
@@ -38,5 +53,33 @@ public partial class ReflectionnaireReport
         {
             Logger.LogError(ex, "Error getting all users answers");
         }
+    }
+
+    private async Task Refresh_Clicked()
+    {
+        await UpdateRadarChart();
+    }
+
+    private Task ShowAnswers_Clicked()
+    {
+        JSRuntime.InvokeVoidAsync("Reflectionnaire.scrollToAnswer", "radarChart").ConfigureAwait(false);
+        
+        return Task.CompletedTask;
+    }
+
+    private void GenerateQRCode(string url)
+    {
+        Url generator = new(url);
+        string payload = generator.ToString();
+
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+        using PngByteQRCode qrCode = new(qrCodeData);
+        byte[] darkColorRgba = [12, 92, 109, 255]; // reflectionnaire green
+        byte[] lightColorRgba = [0, 0, 0, 0]; // transparant
+        byte[] qrCodeImage = qrCode.GetGraphic(20, darkColorRgba, lightColorRgba);
+
+        string base64 = Convert.ToBase64String(qrCodeImage);
+        _qrBytes = string.Format("data:image/png;base64,{0}", base64);
     }
 }
